cbb0847b3477c304b69ca59dffab7f0946441a33
bakhtiyartayir <bakhtiyarTayir@gmail.com>
2023-03-13 14:02:24 +0500

    init project

---
+++ src/main/java/com/uportal/buysell/BuysellApplication.java
@@ -0,0 +1,12 @@
+package com.uportal.buysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuysellApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BuysellApplication.class, args);
+	}
+
+}
